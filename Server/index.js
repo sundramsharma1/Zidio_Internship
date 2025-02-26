edce24dc2c95f1d114470fff37d570a75c0ac5f7
Sundram Sharma <120078032+sundramsharma1@users.noreply.github.com>
2025-02-26 11:47:51 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@ const feedbackRoute = require('./router/feedback-router');
 const connectDb = require('./utils/db');
 const errorMiddleware = require('./middleware/error-middleware');
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://feedbackcollectionsystem.netlify.app/"], 
+  origin: ["http://localhost:5173", "https://feedbackcollectionsystem.netlify.app"], 
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true, 
 };
