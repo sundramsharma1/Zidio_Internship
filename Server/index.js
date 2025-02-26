369e2b5214849f3036439ae7d1ab48bda95d2465
Sundram Sharma <120078032+sundramsharma1@users.noreply.github.com>
2025-02-26 10:05:34 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 const authRoute = require('./router/auth-router');
 const contactRoute = require('./router/contact-router');
 const serviceRoute = require('./router/service-router');
